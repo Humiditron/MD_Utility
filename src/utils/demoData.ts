b922df7d9e8d41eb8ab8a108066b8c3ed5c079ed
PurePHOS <223341941+Humiditron@users.noreply.github.com>
2026-08-23 18:37:24 -0400

    fixed header rendering.
    fixed herder rendering.

--- src/utils/demoData.ts
+++ src/utils/demoData.ts
@@ -196,6 +196,33 @@ Do not run root-privileged containers in multi-tenant environments.
 </Warning>`,
     },
     {
+      path: `${repoFolder}/docs/advanced/async-tests.md`,
+      content: `## pytest.mark.anyio { #pytest-mark-anyio }
+
+If we want to call asynchronous functions in our tests, our test functions have to be asynchronous. AnyIO provides a neat plugin for this, that allows us to specify that some test functions are to be called asynchronously.
+
+## HTTPX { #httpx }
+
+Even if your **FastAPI** application uses normal \`def\` functions instead of \`async def\`, it is still an async application underneath.
+
+The \`TestClient\` does some magic inside to call the asynchronous FastAPI application in your normal \`def\` test functions, using standard pytest. But that magic doesn't work anymore when we're using it inside asynchronous functions. By running our tests asynchronously, we can no longer use the \`TestClient\` inside our test functions.
+
+The \`TestClient\` is based on [HTTPX](https://www.python-httpx.org), and luckily, we can use it directly to test the API.
+
+## Example { #example }
+
+For a simple example, let's consider a file structure similar to the one described in [Bigger Applications](../tutorial/bigger-applications.md) and [Testing](../tutorial/testing.md):
+
+\`\`\`
+.
+├── app
+│   ├── __init__.py
+│   ├── main.py
+│   └── test_main.py
+\`\`\`
+`,
+    },
+    {
       path: `${repoFolder}/src/index.ts`, // Non-md file to test filtering
       content: `console.log("Acme engine booted");`,
     },
